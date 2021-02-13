b6c6b46da45850c6bd6bd1976cd40d52375ad500
demoraes <gabrielmoraes_2013@outlook.com>
2021-02-13 13:02:57 -0400

    Rota e controller de perfil

---
+++ backend/src/modules/users/services/ShowProfileService.spec.ts
@@ -0,0 +1,50 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import ShowProfileService from './ShowProfileService';
+import UpdateProfile from './UpdateProfileService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let showProfile: ShowProfileService;
+let updateProfile: UpdateProfile;
+
+describe('UpdateProfile', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+
+    showProfile = new ShowProfileService(fakeUsersRepository);
+  });
+
+  it('should be able show the profile', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    const profile = await showProfile.execute({
+      user_id: user.id,
+    });
+
+    expect(profile.name).toBe('John Doe');
+    expect(profile.email).toBe('johndoe@example.com');
+  });
+
+  it('should not be able update the profile from non-existing user', async () => {
+    expect(
+      updateProfile.execute({
+        user_id: 'non-existing-user-id',
+        name: 'Test',
+        email: 'test@example.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able show the profile from non-existing user', async () => {
+    expect(
+      showProfile.execute({
+        user_id: 'non-existing-user-id',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
