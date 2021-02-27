1e9b4be65f752fde652cfa6763bd1257c9dfb615
demoraes <gabrielmoraes_2013@outlook.com>
2021-02-27 01:49:13 -0400

    Endereço das imagens

--- backend/src/modules/appointments/services/ListProvidersService.ts
+++ backend/src/modules/appointments/services/ListProvidersService.ts
@@ -24,13 +24,15 @@ class ListProvidersService {
       `providers-list:${user_id}`,
     );
 
+    // let users = null;
+
     if (!users) {
       users = await this.usersRepository.findAllProviders({
         except_user_id: user_id,
       });
-
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
     }
+
+    await this.cacheProvider.save(`providers-list:${user_id}`, users);
 
     return users;
   }
